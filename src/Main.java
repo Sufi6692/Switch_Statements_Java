eae92b35d71e21cfa1250b066d6b5b437f8f31fc
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-09 22:27:16 +0530

    Understand the Enhanced switch statement using Fruits cases

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,33 @@ public  class Main {
         System.out.print("Enter your fruit : ");
         String fruit = input.next();
 
-        switch (fruit){
-            case "Mango":
-                System.out.println("King of fruits");
-                break;
-            case "Apple":
-                System.out.println("A sweet red fruit");
-                break;
-            case "Orange":
-                System.out.println("Round fruit");
-                break;
-            case "Grapes":
-                System.out.println("Small fruit");
-                break;
-            default:
-                System.out.println("Please enter a valid fruit");
+//        switch (fruit){
+//            case "Mango":
+//                System.out.println("King of fruits");
+//                break;
+//            case "Apple":
+//                System.out.println("A sweet red fruit");
+//                break;
+//            case "Orange":
+//                System.out.println("Round fruit");
+//                break;
+//            case "Grapes":
+//                System.out.println("Small fruit");
+//                break;
+//            default:
+//                System.out.println("Please enter a valid fruit");
+//        }
+
+
+
+        //Enhanced switch
+
+        switch (fruit) {
+            case "Mango" -> System.out.println("King of fruits");
+            case "Apple" -> System.out.println("A sweet red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            case "Grapes" -> System.out.println("Small fruit");
+            default -> System.out.println("Please enter a valid fruit");
         }
     }
 }
