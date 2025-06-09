a5df770f020486892d52a0671d2a1f7fd605bf9f
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-09 22:22:02 +0530

    Understand the switch statement using Fruits cases

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public  class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your fruit : ");
+        String fruit = input.next();
+
+        switch (fruit){
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            case "Grapes":
+                System.out.println("Small fruit");
+                break;
+            default:
+                System.out.println("Please enter a valid fruit");
         }
     }
 }
